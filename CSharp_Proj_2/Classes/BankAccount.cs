cb76a66b52cd297df3fa4d12dcbe5eece183e606
vkatdare <veena.katdare@gmail.com>
2022-03-02 17:36:35 -0500

    new files added

---
+++ CSharp_Proj_2/Classes/BankAccount.cs
@@ -0,0 +1,87 @@
+﻿using System;
+namespace CSharp_Proj_2.Classes
+{
+    public class BankAccount
+     
+    {
+        private string owner;
+        private float balance;
+
+        public float Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            private set {
+                if (value >= 0)
+                {
+                    balance = value;
+
+                }
+                else
+                {
+                    balance = 0;
+                }
+
+            }
+
+        }
+
+
+        public BankAccount(string owner, float balance)
+        {
+            this.owner = owner;
+            this.Balance = balance;
+        }
+
+        public virtual  float AddBalance(float newBalance)
+        {
+            Balance = balance + newBalance;
+
+            return Balance;
+        }
+
+        public virtual float AddBalance(float newBalance, bool balanceCanBeNeg)
+        {
+            if (balanceCanBeNeg)
+            {
+                balance = balance + newBalance;
+            }
+            else
+            {
+
+                Balance = Balance + newBalance;
+            }
+
+            return Balance;
+        }
+    }
+
+    public class ChildBankAccount : BankAccount
+    {
+        public string Parent { get; set; }
+
+        public ChildBankAccount(string owner, float balance, string parent) : base(owner, balance)
+        {
+            Parent = parent;
+        }
+
+        public override float AddBalance(float newBalance)
+        {
+            if (newBalance >= -10)
+                return base.AddBalance(newBalance);
+            else
+                return Balance;
+        }
+
+        public override float AddBalance(float newBalance, bool balanceCanBeNeg)
+        {
+            if (newBalance >= -10)
+                return base.AddBalance(newBalance, balanceCanBeNeg);
+            else
+                return Balance;
+        }
+    }
+}
---
+++ CSharp_Proj_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using CSharp_Proj_2.Classes;
+
+namespace CSharp_Proj_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount1 = new BankAccount("Jane", 125461f);
+            ChildBankAccount bankAccount2 = new ChildBankAccount("Bob", 1334f, "Jane");
+
+            Console.WriteLine(bankAccount1.Balance);
+
+            Console.WriteLine(bankAccount2.Balance);
+
+            Console.WriteLine(bankAccount1.AddBalance(456f));
+
+            Console.WriteLine(bankAccount2.AddBalance(-143f, true));
+
+            Console.WriteLine(bankAccount2.AddBalance(-9f, true));
+        }
+    }
+
+    class SimpleMath
+    {
+
+        public static float Add(float n1, float n2) {
+
+            return n1 + n2;
+
+        }
+
+        public static float Subtract(float n1, float n2)
+        {
+
+            return n1 - n2;
+
+        }
+        public static float Mult(float n1, float n2)
+        {
+
+            return n1 * n2;
+
+        }
+        public static float Divide(float n1, float n2)
+        {
+
+            return n1 / n2;
+
+        }
+
+    }
+}
