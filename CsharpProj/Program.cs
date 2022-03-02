467bff47cde9224b85e6c69838bc02008b4a656d
vkatdare <veena.katdare@gmail.com>
2022-03-02 13:35:19 -0500

    methods

--- CsharpProj/Program.cs
+++ CsharpProj/Program.cs
@@ -4,9 +4,39 @@ namespace CsharpProj
 {
     class Program
     {
+        static float n1 = 234, n2 = 345;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Add());
+            Console.WriteLine(Subtract());
+            Console.WriteLine(Mult());
+            Console.WriteLine(Divide());
+        }
+
+        private static float Add()
+        {
+            return n1 + n2;
+           
+        }
+
+        private static float Subtract()
+        {
+            var result = n1 - n2;
+            return result;
+        }
+
+        private static float Mult()
+        {
+            var result = n1 * n2;
+            return result;
+        }
+
+        private static float Divide()
+        {
+            var result = n1 / n2;
+            return result;
         }
     }
+
 }
